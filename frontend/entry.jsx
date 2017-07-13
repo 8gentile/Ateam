5f209d9644c497fcd64ebac86d0b31349c18852c
8gentile <endureliberty@yahoo.com>
2017-07-13 15:25:03 -0400

    Remove testing areas from entry file

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -8,29 +8,16 @@ import {
   Link
 } from 'react-router-dom';
 
-///// testing imports
-import { fetchTeams, createTeam } from './actions/user_actions';
-/////
-
 document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
 
-let store;
-if (window.currentUser) {
-  const preloadedState = { session: { currentUser: window.currentUser } };
-  store = configureStore(preloadedState);
-  delete window.currentUser;
-} else {
-  store = configureStore();
-}
-
-//Window Testing Area
-window.dispatch = store.dispatch;
-window.getState = store.getState;
-//////////////////// temp tests
-window.fetchTeams = fetchTeams;
-window.createTeam = createTeam;
-////////////////////
-
-const root = document.getElementById('root');
-ReactDOM.render(<Root store={store} />, root);
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
 });
