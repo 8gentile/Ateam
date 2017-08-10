9b1c9c15694eea563740b99148c6785bccab126e
8gentile <endureliberty@yahoo.com>
2017-08-10 16:18:01 -0400

    Reconfigure store to not display redux logger during production

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,12 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import rootReducer from '../reducers/root_reducer';
+import RootReducer from '../reducers/root_reducer';
 
-export default ( preloadedState = {} ) => {
-  return createStore(
-    rootReducer,
-    preloadedState,
-    applyMiddleware(thunk, logger)
-  );
-};
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const createLogger = require('redux-logger');
+  middlewares.push(createLogger());
+}
+
+const configureStore = (preloadedState = {}) => (
+  createStore(RootReducer, preloadedState, applyMiddleware(...middlewares))
+);
+
+export default configureStore;
