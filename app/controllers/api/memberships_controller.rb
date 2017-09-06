0fd072787cbb04ed424517399ae8166604b3392e
8gentile <endureliberty@yahoo.com>
2017-09-06 14:38:06 -0400

    Update invitation mailer

--- app/controllers/api/memberships_controller.rb
+++ app/controllers/api/memberships_controller.rb
@@ -1,8 +1,8 @@
 class Api::MembershipsController < ApplicationController
 
   def create
-    # @user_id = User.find_user_id_by_email(params[:email][:member][:email])
-    @user = User.find_by_email(params[:email][:member][:email])
+    @email = params[:email][:member][:email]
+    @user = User.find_by_email(@email)
     if @user
       @membership = Membership.new({ user_id: @user.id, team_id: params[:email][:member][:team_id] })
       if @membership.save
@@ -12,8 +12,8 @@ class Api::MembershipsController < ApplicationController
         render json: @membership.errors.full_messages, status: 422
       end
     else
-      InviteMailer.invitation_email(params[:email][:member][:email]).deliver_now
-      Invite.create({email: params[:email][:member][:email], team_id: params[:email][:member][:team_id], pending: true})
+      InviteMailer.invitation_email(@email).deliver_now
+      Invite.create({email: @email, team_id: params[:email][:member][:team_id], pending: true})
       @users = current_user.teammates
       render 'api/users/show'
     end
